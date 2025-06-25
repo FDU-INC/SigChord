99e1ea8925b6e26aebe49ee18d21f7b41965b13b
umdetree <umdetree@gmail.com>
2025-06-25 13:56:45 +0800

    modify the LDPC parity matrices path

--- scripts/matlab_ffi/setupDvbs2.m
+++ scripts/matlab_ffi/setupDvbs2.m
@@ -1,5 +1,5 @@
 %% DVB-S2 config
-addpath('../s2xLDPCParityMatrices');
+addpath('./s2xLDPCParityMatrices');
 if ~exist('../dvbs2xLDPCParityMatrices.mat','file')
     if ~exist('../s2xLDPCParityMatrices.zip','file')
         url = 'https://ssd.mathworks.com/supportfiles/spc/satcom/DVB/s2xLDPCParityMatrices.zip';
