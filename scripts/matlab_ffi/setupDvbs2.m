ffec067a0bb75352788d17d527f33215c831b179
umdetree <umdetree@gmail.com>
2025-08-06 15:44:52 +0800

    fix path bug in setupDvbs2.m

--- scripts/matlab_ffi/setupDvbs2.m
+++ scripts/matlab_ffi/setupDvbs2.m
@@ -1,10 +1,9 @@
 %% DVB-S2 config
-addpath('./s2xLDPCParityMatrices');
-if ~exist('../dvbs2xLDPCParityMatrices.mat','file')
-    if ~exist('../s2xLDPCParityMatrices.zip','file')
+if ~exist('./dvbs2xLDPCParityMatrices.mat','file')
+    if ~exist('./s2xLDPCParityMatrices.zip','file')
         url = 'https://ssd.mathworks.com/supportfiles/spc/satcom/DVB/s2xLDPCParityMatrices.zip';
-        websave('../s2xLDPCParityMatrices.zip',url);
-        unzip('../s2xLDPCParityMatrices.zip');
+        websave('./s2xLDPCParityMatrices.zip',url);
+        unzip('./s2xLDPCParityMatrices.zip');
     end
-addpath('../s2xLDPCParityMatrices');
+addpath('./s2xLDPCParityMatrices');
 end
